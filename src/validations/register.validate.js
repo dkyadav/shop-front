3391cee7a3b3ce9e2f90a48bad0127853d73cb79
Deepak Yadav <deepak.yadav@linkmobility.com>
2024-04-20 12:21:01 +0530

    joi change

--- src/validations/register.validate.js
+++ src/validations/register.validate.js
@@ -2,7 +2,7 @@
 import Joi from "joi";
 
 export const RegisterSchema = {
-    name: Joi.string().alphanum().min(3).max(30).required(),
+    name: Joi.string().min(3).max(30).required(),
 
     email: Joi.string().email({
       minDomainSegments: 2,
